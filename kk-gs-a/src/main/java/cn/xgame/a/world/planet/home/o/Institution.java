65cf5f9e7cc2acb070a422e54bccde2e6c89222d
deng <dell_@dell_-PC>
2015-08-05 18:58:27 +0800

    1

--- kk-gs-a/src/main/java/cn/xgame/a/world/planet/home/o/Institution.java
+++ kk-gs-a/src/main/java/cn/xgame/a/world/planet/home/o/Institution.java
@@ -40,7 +40,7 @@ public enum Institution {
 		@Override
 		public boolean isSenator( int i, int privilege ) {
 			//共和体质：12个话语权最高的人，话语权总和小于50（共和体质允许发起投票的人数在13~18之间，当前18个之前就超过50%按超过50%的最小人数算，反之取前18人）
-			return privilege <= 5000 && i < 18;
+			return i < 18;
 		}
 	};
 	
