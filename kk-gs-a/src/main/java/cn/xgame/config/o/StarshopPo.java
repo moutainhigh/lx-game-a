eaa728d4c4bf4d9046e8b27f54cc40451edbc8f3
deng <dell_@dell_-PC>
2015-09-14 18:15:13 +0800

    1

--- kk-gs-a/src/main/java/cn/xgame/config/o/StarshopPo.java
+++
@@ -1,7 +0,0 @@
-package cn.xgame.config.o;import java.util.Map;import x.javaplus.string.StringUtil;public class StarshopPo {	public final int id;	public final String item;
-
-	public StarshopPo( StarshopPo clone ){		this.id = clone.id;		this.item = clone.item;
-
-	}	public StarshopPo( Map<String, String> data ){		id = Integer.parseInt( StringUtil.convertNumberString( data.get("id") ) );		item = data.get("item");
-
-	}}
